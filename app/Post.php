f66ec92bca184e5eeb3e94deceb96331748205c4
Malde Chavda <chavdamaldep@gmail.com>
2019-02-02 14:41:12 +0530

    add media conversion.

--- app/Post.php
+++ app/Post.php
@@ -4,8 +4,10 @@ namespace App;
 
 use App\Traits\CanLike;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\Models\Media;
 
 class Post extends Model implements HasMedia
 {
@@ -39,5 +41,16 @@ class Post extends Model implements HasMedia
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * @param \Spatie\MediaLibrary\Models\Media|null $media
+     *
+     * @throws \Spatie\Image\Exceptions\InvalidManipulation
+     */
+    public function registerMediaConversions(Media $media = null)
+    {
+        $this->addMediaConversion('thumb')
+            ->fit(Manipulations::FIT_CONTAIN, 1200, 1200);
     }
 }
